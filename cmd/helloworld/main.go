a786d25f4d1f2bb055f9770a2310587f2e6b4869
Artur Valiullin <ar.valiullin@yandex.ru>
2024-08-26 00:42:27 +0500

    examples\primitive_types: add complex

--- cmd/helloworld/main.go
+++ cmd/helloworld/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"drogon/internal/helloworld"
+	"fmt"
+	"time"
+)
+
+func myPrint(start, finish int) {
+	for i := start; i < finish; i++ {
+		fmt.Print(i, " ")
+	}
+	fmt.Println()
+	time.Sleep(100 * time.Millisecond)
+}
 
 func main() {
 	fmt.Println("Hello, World!")
+	for i := 0; i < helloworld.COUNT; i++ {
+		go myPrint(i, helloworld.COUNT)
+		time.Sleep(time.Second)
+	}
 }
